aac6356341718d7695904eae7a64362b295218b2
tomoki matsubuchi <matsubuchitmk@gmail.com>
2022-11-19 14:19:00 +0900

    sidekiqでjobが待機状態になり実行されない

--- app/jobs/push_line_job.rb
+++ app/jobs/push_line_job.rb
@@ -11,30 +11,30 @@ class PushLineJob < ApplicationJob
 
 
 
-    #message = {
-    #      type: 'text',
-    #      text: "テストです。"
-    #      }
-    #      response = client.push_message(User.find(1).uid, message)
-
-
-    near_limit_items = ExpendableItem.where(deadline_on: Date.today..Time.now.end_of_day + (2.days))
-    near_limit_items.each do |item|
-      @user = item.user
-      limit_message = {
-        type: 'text',
-        text: "#{item.name}の消費完了が近づいています。"
-        }
-        limit_response = client.push_message(@user.uid, limit_message)
-    end
-
-    other_limit_items = ExpendableItem.where(deadline_on: Date.today..Time.now.end_of_day + (5.days), user_id: @user).where.not(deadline_on: Date.today..Time.now.end_of_day + (2.days))
-      other_limit_items.each do |oth|
-        oth_message = {
+    message = {
           type: 'text',
-          text: "他にも#{oth.name}の消費完了が近いです。"
-        }
-        oth_response = client.push_message(@user.uid, oth_message)
-      end
+          text: "テストです。"
+          }
+          response = client.push_message(User.find(1).uid, message)
+
+
+    #near_limit_items = ExpendableItem.where(deadline_on: Date.today..Time.now.end_of_day + (2.days))
+    #near_limit_items.each do |item|
+    #  @user = item.user
+    #  limit_message = {
+    #    type: 'text',
+    #    text: "#{item.name}の消費完了が近づいています。"
+    #    }
+    #    limit_response = client.push_message(@user.uid, limit_message)
+    #end
+#
+    #other_limit_items = ExpendableItem.where(deadline_on: Date.today..Time.now.end_of_day + (5.days), user_id: @user).where.not(deadline_on: Date.today..Time.now.end_of_day + (2.days))
+    #  other_limit_items.each do |oth|
+    #    oth_message = {
+    #      type: 'text',
+    #      text: "他にも#{oth.name}の消費完了が近いです。"
+    #    }
+    #    oth_response = client.push_message(@user.uid, oth_message)
+    #  end
   end
 end
